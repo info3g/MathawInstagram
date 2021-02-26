7906276eff9e58f20e300b2db698c71e6d45f0db
Dev Team <infothreeg@gmail.com>
2021-02-26 12:28:52 +0530

    Update start.py

--- start.py
+++ start.py
@@ -29,34 +29,34 @@ print(all_tags)
 try:
 	for one_tags in all_tags:
 		try:
-			all_post = L.get_hashtag_posts(one_tags)
-			time.sleep(20)
+			all_post = L.get_hashtag_posts('onlinegroepsles')
+			# time.sleep(20)
 			counts = 0
 			for tags in all_post:
 				try:
 					SHORTCODE = tags.shortcode
 					print("SHORTCODE************************************",counts,"--------------------",SHORTCODE)
-					time.sleep(20)
-					post = Post.from_shortcode(L, SHORTCODE)
-					data= post.__dict__
-					full_node_data = data['_node']
-					user_details = full_node_data['owner']
-					edge_followed_by_count = user_details['edge_followed_by']
-					user_id = user_details['id']
-					user_name =  user_details['username']
-					user_full_name =  user_details['full_name']
-					total_edge_followed_by_count = edge_followed_by_count['count']
-					print("hashtaghashtag________________",hashtag)
-					print('user_id_________________________',user_id)
-					print('user_name_________________________',user_name)
-					print('user_full_name_________________________',user_full_name)
-					print('total_edge_followed_by_count_________________________',total_edge_followed_by_count)
+					time.sleep(5)
+					# post = Post.from_shortcode(L, SHORTCODE)
+					# data= post.__dict__
+					# full_node_data = data['_node']
+					# user_details = full_node_data['owner']
+					# edge_followed_by_count = user_details['edge_followed_by']
+					# user_id = user_details['id']
+					# user_name =  user_details['username']
+					# user_full_name =  user_details['full_name']
+					# total_edge_followed_by_count = edge_followed_by_count['count']
+					# print("hashtaghashtag________________",hashtag)
+					# print('user_id_________________________',user_id)
+					# print('user_name_________________________',user_name)
+					# print('user_full_name_________________________',user_full_name)
+					# print('total_edge_followed_by_count_________________________',total_edge_followed_by_count)
 					counts = counts + 1
-					row = [hashtag, user_name, user_full_name, user_id, total_edge_followed_by_count]
-					with open('pilatesinstructor__result.csv','+a',newline='', encoding='utf-8') as csvFile:
-						writer = csv.writer(csvFile)
-						writer.writerow(row)
-					time.sleep(43)
+					# row = [hashtag, user_name, user_full_name, user_id, total_edge_followed_by_count]
+					# with open('pilatesinstructor__result.csv','+a',newline='', encoding='utf-8') as csvFile:
+					# 	writer = csv.writer(csvFile)
+					# 	writer.writerow(row)
+					# time.sleep(43)
 				except:
 					pass
 		except:
