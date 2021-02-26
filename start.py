c3a58d3f8ca5c235fad9949a73a4bdf2536e3d45
Dev Team <infothreeg@gmail.com>
2021-02-26 12:59:22 +0530

    Update start.py

--- start.py
+++ start.py
@@ -27,25 +27,25 @@ for tags in rows:
 
 SHORTCODES = 'CLvziZEJNGq','CLvyugnppKl','CLvy6LyFDxR','CLvy3YXg6oh'
 for shortcode_all in SHORTCODES:
-try:
-	post = Post.from_shortcode(L, shortcode_all)
-	data= post.__dict__
-	full_node_data = data['_node']
-	user_details = full_node_data['owner']
-	edge_followed_by_count = user_details['edge_followed_by']
-	user_id = user_details['id']
-	user_name =  user_details['username']
-	user_full_name =  user_details['full_name']
-	total_edge_followed_by_count = edge_followed_by_count['count']
-	print("hashtaghashtag________________",hashtag)
-	print('user_id_________________________',user_id)
-	print('user_name_________________________',user_name)
-	print('user_full_name_________________________',user_full_name)
-	print('total_edge_followed_by_count_________________________',total_edge_followed_by_count)
-	row = [hashtag, user_name, user_full_name, user_id, total_edge_followed_by_count]
-	with open('pilatesinstructor__result.csv','+a',newline='', encoding='utf-8') as csvFile:
-		writer = csv.writer(csvFile)
-		writer.writerow(row)
-		time.sleep(15)
-except:
-	pass	
+ 	try:
+		post = Post.from_shortcode(L, shortcode_all)
+		data= post.__dict__
+		full_node_data = data['_node']
+		user_details = full_node_data['owner']
+		edge_followed_by_count = user_details['edge_followed_by']
+		user_id = user_details['id']
+		user_name =  user_details['username']
+		user_full_name =  user_details['full_name']
+		total_edge_followed_by_count = edge_followed_by_count['count']
+		print("hashtaghashtag________________",hashtag)
+		print('user_id_________________________',user_id)
+		print('user_name_________________________',user_name)
+		print('user_full_name_________________________',user_full_name)
+		print('total_edge_followed_by_count_________________________',total_edge_followed_by_count)
+		row = [hashtag, user_name, user_full_name, user_id, total_edge_followed_by_count]
+		with open('pilatesinstructor__result.csv','+a',newline='', encoding='utf-8') as csvFile:
+			writer = csv.writer(csvFile)
+			writer.writerow(row)
+			time.sleep(15)
+	except:
+		pass	
